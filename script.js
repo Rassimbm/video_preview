fa2a9dc87d0d17f6bd11f033b9dfa75c0440914f
Rassimbm <rassimbenmhamed@outlook.com>
2023-10-09 13:33:36 -0500

    added a play and pause functions to script

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("page loaded...");
+
+function playVideo(video) {
+    video.play();
+}
+
+function pauseVideo(video) {
+    video.pause();
+    // video.currentTime = 0;
+}
+
+playVideo();
+pauseVideo();
